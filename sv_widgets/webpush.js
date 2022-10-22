3ce9d7bf1cff8187daf45cbff7aa9ca66b34fc33
gruberth <gruaber@hotmail.de>
2022-10-22 11:13:55 +0200

    Copied plugin to own repo

---
+++ sv_widgets/webpush.js
@@ -0,0 +1,200 @@
+let webpushwidget = null;
+
+$.widget("sv.webpush", $.sv.widget, {
+    initSelector: 'div[data-widget="webpush.config"]',
+
+    applicationServerPublicKey: '',
+    pushButton: null,
+    swRegistration: null,
+
+    // console logging
+    verbose: false,
+
+    options: {
+		buttontext: 'Übernehmen',
+	},
+
+    _create: function () {
+        this._super();
+        this.widgetdiv = this.element[0];
+
+        webpushwidget = this;
+
+        this.pushButton = document.getElementById('webpush_config_submitbutton');
+        this.registerWorker();
+    },
+
+    _update: function (response) {
+        this.applicationServerPublicKey = response[1];
+
+        var div = document.getElementById('webpush_config_checkboxdiv');
+        div.innerHTML = "";
+        var groupslist = JSON.parse(localStorage.getItem('webpush_groups'));
+        if (groupslist === null){
+            groupslist = []
+        }
+
+        for (const group of response[0]) {
+            var checkboxdiv = document.createElement('div');
+            checkboxdiv.id = 'checkboxdiv_' + group;
+            checkboxdiv.className = "ui-checkbox";
+
+            var label = document.createElement('label');
+            label.className = "ui-btn ui-corner-all ui-btn-inherit ui-btn-icon-left";
+            label.id = 'checkboxlabel_' + group;
+            label.innerText = group;
+
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = 'checkbox_' + group;
+            checkbox.name = 'webpush_config_checkbox';
+            checkbox.value = group;
+            if (groupslist.includes(group)) {
+                checkbox.checked = true;
+            }
+
+            label.appendChild(checkbox);
+            checkboxdiv.appendChild(label);
+            div.appendChild(checkboxdiv);
+        }
+    },
+
+    urlB64ToUint8Array: function (base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+    },
+
+    updateSubscription: function (subscription) {
+        var sessionId = localStorage.getItem('webpush_sessionID');
+        if (sessionId === null) {
+            sessionId = "" + Date.now();
+            localStorage.setItem('webpush_sessionID', sessionId);
+        }
+
+        var groupslist = [];
+        var checkboxes = document.getElementsByName('webpush_config_checkbox');
+        for (var i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].checked) {
+                    groupslist.push(checkboxes[i].value);
+                }
+        }
+
+        localStorage.setItem('webpush_groups', JSON.stringify(groupslist));
+
+        var message = {
+            sessionId: sessionId,
+            groups: groupslist
+        };
+
+        if (groupslist === [] || subscription === '') {
+            message.cmd = "unsubscribe";
+            message.subscription = "";
+        } else {
+            message.cmd = "subscribe";
+            message.subscription = subscription;
+        }
+
+        if (this.verbose)
+            console.log(JSON.stringify(message));
+
+        var items = this.options.item.explode();
+        io.write(items[2], JSON.stringify(message));
+    },
+
+    unsubscribeUser: function () {
+        this.swRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+                if (subscription) {
+                    return subscription.unsubscribe();
+                }
+            })
+            .catch(function (error) {
+                console.log('Error unsubscribing', error);
+            })
+            .then(function () {
+                if (webpushwidget.verbose)
+                    console.log('User is unsubscribed.');
+                webpushwidget.updateSubscription('');
+            });
+    },
+
+    subscribeUser: function () {
+        const applicationServerKey = this.urlB64ToUint8Array(this.applicationServerPublicKey);
+        this.swRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: applicationServerKey
+        })
+            .then(function (subscription) {
+                if (webpushwidget.verbose)
+                    console.log('User is subscribed.');
+                webpushwidget.updateSubscription(subscription);
+            })
+            .catch(function (error) {
+                console.error('Failed to subscribe the user: ', error);
+            });
+        return ""
+    },
+
+    initializeUI: function () {
+        this.swRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+                if (Notification.permission === 'denied') {
+                    webpushwidget.pushButton.textContent = 'Push Messaging Blocked';
+                } else {
+                    if (subscription === null) {
+                        if (webpushwidget.verbose)
+                            console.log('User is NOT subscribed.');
+                    } else {
+                        if (webpushwidget.verbose)
+                            console.log('User IS subscribed.');
+                    }
+
+                    webpushwidget.pushButton.textContent = webpushwidget.options.buttontext;
+                    webpushwidget.pushButton.addEventListener('click', function () {
+                        var groupslist = [];
+                        var checkboxes = document.getElementsByName('webpush_config_checkbox');
+                        for (var i = 0; i < checkboxes.length; i++) {
+                                if (checkboxes[i].checked) {
+                                    groupslist.push(checkboxes[i].value);
+                                }
+                        }
+                        if (groupslist.length > 0){
+                            webpushwidget.subscribeUser();
+                        } else{
+                            webpushwidget.unsubscribeUser();
+                        }
+                    });
+                }
+            });
+    },
+
+    registerWorker: function () {
+        if ('serviceWorker' in navigator && 'PushManager' in window) {
+            if (webpushwidget.verbose)
+                console.log('Service Worker and Push are supported');
+            navigator.serviceWorker.register('dropins/widgets/webpush_serviceworker.js')
+                .then(function (swReg) {
+                    if (webpushwidget.verbose)
+                        console.log('Service Worker is registered', swReg);
+                    webpushwidget.swRegistration = swReg;
+                    webpushwidget.initializeUI();
+                })
+                .catch(function (error) {
+                    console.error('Service Worker Error', error);
+                });
+        } else {
+            console.warn('Push messaging is not supported');
+            this.pushButton.textContent = 'Push Not Supported';
+        }
+    }
+});
---
+++ user_doc.rst
@@ -0,0 +1,107 @@
+webpush
+=======
+
+Plugin zum Versenden von web push Nachrichten an Browser Clients
+
+Anforderungen
+-------------
+Auf Client Seite wird ein Browser benötigt der web push Nachrichten unterstützt. Dies ist bei nahezu allen Browsern der
+Fall. Lediglich bei einigen mobilen Android Geräten unterstützt der Standardbrowser diese Funktionalität nicht, wie
+z.B. der MiBrowser auf Xiaomi Android Geräten. Für den größten Funktionsumfang von web push Nachrichten wird jedoch
+der Chrome Browser empfohlen.
+
+ACHTUNG:
+Der Firefox Browser auf Android enthält einen Bug, weshalb web push Nachrichten nicht zugestellt werden. Dazu
+gibt es jedoch schon seit einiger Zeit ein Issue (https://github.com/mozilla-mobile/fenix/issues/19152).
+
+
+Notwendige Software
+~~~~~~~~~~~~~~~~~~~
+
+Für die Kommunikation wird die Python-Bibliothek pywebpush benötigt. Des weiteren wird py-vapid für die Schlüssel
+Verwaltung benutzt.
+
+Diese werden bei der ersten Verwendung des Plugins automatisch zu SmarthomeNG hinzugefügt.
+
+Konfiguration
+-------------
+
+plugin.yaml
+~~~~~~~~~~~
+
+Bitte die Dokumentation lesen, die aus den Metadaten der plugin.yaml erzeugt wurde.
+
+.. code-block:: yaml
+
+    webpush:
+    plugin_name: webpush
+    grouplist:
+    -   alarm
+    -   info
+
+Items
+~~~~~
+
+Für die Kommunikation und die Übertragung der Einstellungen vom Plugin an die Visu des Clients müssen die Items aus dem
+webpush.basic Struct in der Konfiguration enthalten sein, ohne diese startet das Plugin nicht. Weiters sind diese
+drei Items auch die notwendigen Parameter für das webpush.config Widget.
+
+Funktionen
+~~~~~~~~~~
+
+Logik Funktion zum senden einer Nachricht an eine Gruppe:
+
+**sendPushNotification(msg, group, title, ...)**
+
+Für eine genaue Beschreibung aller Parameter, bitte die aus der plugin.yaml erzeugte Dokumentation beachten.
+Infos zum web push Standard sind unter folgenden Links zu finden:
+
+https://www.rfc-editor.org/rfc/rfc8030.txt
+
+https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+
+https://developer.mozilla.org/en-US/docs/Web/API/Push_API
+
+Weitere Infos zum highpriority Parameter der die Urgency von normal auf high stellt:
+
+   +----------+-----------------------------+--------------------------+
+   | Urgency  | Device State                | Example Application      |
+   |          |                             | Scenario                 |
+   +----------+-----------------------------+--------------------------+
+   | very-low | On power and Wi-Fi          | Advertisements           |
+   | low      | On either power or Wi-Fi    | Topic updates            |
+   | normal   | On neither power nor Wi-Fi  | Chat or Calendar Message |
+   | high     | Low battery                 | Incoming phone call or   |
+   |          |                             | time-sensitive alert     |
+   +----------+-----------------------------+--------------------------+
+
+SV Widget
+---------
+
+Nachfolgend sind die Parameter für das Widget aufgelistet.
+
+.. code-block:: html
+
+    {{ webpush.config(id, grouplist, publickey, fromclient, buttontext) }}
+
+Eine Beispielhafte Verwendung könnte dabei so aussehen:
+
+.. code-block:: html
+
+    {{ webpush.config('', 'webpush.config.grouplist', 'webpush.config.publickey', 'webpush.comunication.fromclient', 'Übernehmen') }}
+
+
+Web Interface
+-------------
+
+Im Webinterface werden die Grundlegenden Parameter des Plugins angezeigt. Weiters ist dort eine Auflistung der Anzahl an
+Abonnenten pro Gruppe gezeigt. Über einen Button kann die Datenbank geleert werden. Achtung dadurch werden alle
+Abonnenten gelöscht und können nicht wiederhergestellt werden, jeder Client muss sich erneut Nachrichten Gruppen
+abonnieren.
+
+
+Credits
+-------
+
+* SmartHome NG Team
+* WebPush libraries Team (https://github.com/web-push-libs) and their [pywebpush](https://github.com/web-push-libs/pywebpush) and [py-vapid](https://github.com/web-push-libs/vapid) projects)
